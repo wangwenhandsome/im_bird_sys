e0827b482191ea12ed6bfc527de0770bd4345870
wangwenhandsome <wangwen_0513@163.com>
2020-04-22 22:19:52 +0800

    birdchat

--- src/main/java/org/wdzl/bo/UserBO.java
+++ src/main/java/org/wdzl/bo/UserBO.java
@@ -8,5 +8,22 @@ package org.wdzl.bo;
  */
 
 public class UserBO {
+    private String userId;
+    private String faceData;
 
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFaceData() {
+        return faceData;
+    }
+
+    public void setFaceData(String faceData) {
+        this.faceData = faceData;
+    }
 }
--- src/main/java/org/wdzl/controller/UserController.java
+++ src/main/java/org/wdzl/controller/UserController.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.wdzl.bo.UserBO;
 import org.wdzl.pojo.User;
 import org.wdzl.srevices.UserServices;
+import org.wdzl.utils.FastDFSClient;
+import org.wdzl.utils.FileUtils;
 import org.wdzl.utils.IWdzlJSONResult;
 import org.wdzl.utils.MD5Utils;
 import org.wdzl.vo.UserVo;
@@ -23,6 +27,10 @@ import org.wdzl.vo.UserVo;
 public class UserController {
     @Autowired
     UserServices userServices;
+
+    @Autowired
+    FastDFSClient fastDFSClient;
+
     @RequestMapping("/getUser")
     public String getUserById(String id, Model model){
         User user= userServices.getUserById(id);
@@ -52,7 +60,33 @@ public class UserController {
         BeanUtils.copyProperties(userResult,userVo);
         return IWdzlJSONResult.ok(userVo);
     }
+    @RequestMapping("/uploadFaceBase64")
+    @ResponseBody
+    //用户头像上传访问
+    public IWdzlJSONResult registerOrlogin(UserBO userBO) throws Exception {
+        //获取前端传过来的Base64的字符串，然后转化为文件对象在进行上传
+        String base64Data=userBO.getFaceData();
+        String userFacePath="D:\\"+userBO.getUserId()+"userFaceBase64.png";
+        //调用FileUtils类的方法将base64字符串转为文件对象
+        FileUtils.base64ToFile(userFacePath,base64Data);
+        MultipartFile multipartFile = FileUtils.fileToMultipart(userFacePath);
+        //获取fastDFS上传图片的路径
+        String url= fastDFSClient.uploadBase64(multipartFile);
+        System.out.println(url);
+        String thump="_150×150.";
+        String[] arr = url.split("\\.");
+        String thumpImgUrl=arr[0]+thump+arr[1];
 
+//        String bigllFace ="sddsdsdsdcx.png";
+//        String smallFace ="sddsdsdsdcx_150×150.png";
+        //更新用户头像
+        User user=new User();
+        user.setId(userBO.getUserId());
+        user.setFaceImage(thumpImgUrl);
+        user.setFaceImageBig(url);
+        User result = userServices.updateUserinfo(user);
+        return IWdzlJSONResult.ok(result);
+    }
     @RequestMapping("/setNickname")
     @ResponseBody
     //修改昵称方法
