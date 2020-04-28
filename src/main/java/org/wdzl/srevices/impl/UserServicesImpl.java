284e92b6f9e5f54f79062a870fd5953f992d9645
wangwenhandsome <wangwen_0513@163.com>
2020-04-28 20:46:45 +0800

    birdchat

--- src/main/java/org/wdzl/srevices/impl/UserServicesImpl.java
+++ src/main/java/org/wdzl/srevices/impl/UserServicesImpl.java
@@ -3,9 +3,15 @@ package org.wdzl.srevices.impl;
 import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.wdzl.mapper.UserMapper;
 import org.wdzl.pojo.User;
 import org.wdzl.srevices.UserServices;
+import org.wdzl.utils.FastDFSClient;
+import org.wdzl.utils.FileUtils;
+import org.wdzl.utils.QRCodeUtils;
+
+import java.io.IOException;
 
 /**
  * @Author: 王文
@@ -18,8 +24,16 @@ public class UserServicesImpl implements UserServices {
     //注入Mapper
     @Autowired
     UserMapper userMapper;
+
     @Autowired
     Sid sid;
+
+    @Autowired
+    QRCodeUtils qrCodeUtils;
+
+    @Autowired
+    FastDFSClient fastDFSClient;
+
     @Override
     public User getUserById(String id) {
         return userMapper.selectByPrimaryKey(id);
@@ -32,7 +46,20 @@ public class UserServicesImpl implements UserServices {
 
     @Override
     public User insert(User user) {
-         user.setId(sid.nextShort());
+         String userId=sid.nextShort();
+         //为每一个注册用户生成一个唯一的二维码
+        String qrCodePath="D://user"+userId+"qrcode.png";
+        //创建二维码对象信息
+        qrCodeUtils.createQRCode(qrCodePath,"bird_qrcode"+user.getUsername());
+        MultipartFile qrcodeFile = FileUtils.fileToMultipart(qrCodePath);
+        String qrCodeURl ="";
+        try {
+            qrCodeURl = fastDFSClient.uploadQRCode(qrcodeFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        user.setId(userId);
+        user.setQrcode(qrCodeURl);
          userMapper.insert(user);
          return user;
     }
