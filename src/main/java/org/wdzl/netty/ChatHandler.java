a60a47e74ec025b264621503a21517bf9e0e368c
wangwenhandsome <wangwen_0513@163.com>
2020-05-31 08:17:59 +0800

    birdchat

--- src/main/java/org/wdzl/netty/ChatHandler.java
+++ src/main/java/org/wdzl/netty/ChatHandler.java
@@ -18,30 +18,36 @@ import java.time.LocalDateTime;
 
 public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
     //用于记录和管理所有客户端的channel
-    private static ChannelGroup clients=new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    private static ChannelGroup users=new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
         //获取客户端所传输的消息
         String content = msg.text();
-        System.out.println("接受到的数据："+content);
-
-        //将数据刷新到客户端上
-        clients.writeAndFlush(
-                new TextWebSocketFrame(
-                        "[服务器在：]"+ LocalDateTime.now()
-                        +"接受到的消息，消息内容为："+content));
+        //1.获取客户端的发来的消息
+        //2.判断消息的类型,根据不同的类型处理不同的业务
+        /**
+         * 2.1当websocket第一次open,初始化channel，把用的channel和userid关联
+         * 2.2聊天类型的消息，把聊天记录保存到数据库，同时标记消息的签收状态【未签收】
+         * 2.3签收消息类型，针对具体的消息进行签收，修改数据库中对应的消息的签身状态【已签收】
+         */
     }
 
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-        clients.add(ctx.channel());
+        users.add(ctx.channel());
     }
 
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
-        //clients.remove(ctx.channel());
-        System.out.println("客户端断开，channel对应的长Id为:"+ctx.channel().id().asLongText());
-        System.out.println("客户端断开，channel对应的短Id为:"+ctx.channel().id().asShortText());
+        users.remove(ctx.channel());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        //发生异常后关闭连接，同时从channelgroup里面进
+        ctx.channel().close();
+        users.remove(ctx.channel());
     }
 }
