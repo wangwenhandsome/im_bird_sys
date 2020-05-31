0361406873c1cd8882542ca85c234a8b45296f03
wangwenhandsome <wangwen_0513@163.com>
2020-05-31 08:44:15 +0800

    birdchat

---
+++ src/main/java/org/wdzl/netty/ChatMsg.java
@@ -0,0 +1,14 @@
+package org.wdzl.netty;
+
+import java.io.Serializable;
+
+/**
+ * @Author: 王文
+ * @Date: 2020/5/31 8:43
+ * @Version: 1.0
+ * @Description:
+ */
+
+public class ChatMsg implements Serializable {
+
+}
--- src/main/java/org/wdzl/netty/DataContent.java
+++ src/main/java/org/wdzl/netty/DataContent.java
@@ -1,5 +1,7 @@
 package org.wdzl.netty;
 
+import java.io.Serializable;
+
 /**
  * @Author: 王文
  * @Date: 2020/5/31 8:41
@@ -7,5 +9,8 @@ package org.wdzl.netty;
  * @Description:
  */
 
-public class DataContent {
+public class DataContent implements Serializable {
+    private Integer action;//动作类型
+    private ChatMsg chatMsg;//用户的聊天内容
+    private String extand;//扩展字段
 }
