a24135e4c82b3291ab6572be52c903e127600a48
wangwenhandsome <wangwen_0513@163.com>
2020-05-31 16:43:20 +0800

    birdchat

--- src/main/java/org/wdzl/ImBirdSysApplication.java
+++ src/main/java/org/wdzl/ImBirdSysApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 
@@ -12,6 +13,11 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan(value = "org.wdzl.mapper")
 @ComponentScan(basePackages = {"org.wdzl","org.n3r.idworker"})
 public class ImBirdSysApplication extends SpringBootServletInitializer {
+    @Bean
+    public SpringUtil getSpringutil(){
+        return new SpringUtil();
+    }
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(ImBirdSysApplication.class);
