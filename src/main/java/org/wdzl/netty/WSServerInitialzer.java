bdaa0232f66614e0cef0142e914ff7f680365049
wangwenhandsome <wangwen_0513@163.com>
2020-06-21 22:53:11 +0800

    birdchat

--- src/main/java/org/wdzl/netty/WSServerInitialzer.java
+++ src/main/java/org/wdzl/netty/WSServerInitialzer.java
@@ -7,6 +7,7 @@ import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.handler.timeout.IdleStateHandler;
 
 public class WSServerInitialzer extends ChannelInitializer<SocketChannel> {
     @Override
@@ -20,6 +21,16 @@ public class WSServerInitialzer extends ChannelInitializer<SocketChannel> {
         //对httpMessage 进行聚合处理，聚合成request或 response
         pipeline.addLast(new HttpObjectAggregator(1024*64));
 
+        //===========================增加心跳支持==============================
+
+        /**
+         * 针对客户端，如果在1分钟时间内没有向服务端发送读写心跳（ALL），则主动断开连接
+         * 如果有读空闲和写空闲，则不做任何处理
+         */
+        pipeline.addLast(new IdleStateHandler(8,10,12));
+        //自定义的空闲状态检测的handler
+        pipeline.addLast(new HeartBeatHandler());
+
         /**
          * 本handler 会帮你处理一些繁重复杂的事情
          * 会帮你处理握手动作：handshaking（close、ping、pong） ping+pong = 心跳
@@ -29,6 +40,9 @@ public class WSServerInitialzer extends ChannelInitializer<SocketChannel> {
 
         //自定义的handler
         pipeline.addLast(new ChatHandler());
+
+
+
 
     }
 }
