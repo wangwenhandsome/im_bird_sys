d2515db0edcbbee24df1d32101c2de2a4101eaf3
wangwenhandsome <wangwen_0513@163.com>
2020-06-22 11:09:04 +0800

    birdchat

---
+++ src/main/java/org/wdzl/WarStartApplication.java
@@ -0,0 +1,15 @@
+package org.wdzl;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * 使用外置tomcat 启动ImBirdSysApplication
+ */
+public class WarStartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ImBirdSysApplication.class);
+    }
+}
